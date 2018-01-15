f76d98aff542075494ef49f2a33a5358480b1299
26278 <26278@DESKTOP-47DLIGG>
2018-01-15 08:47:20 +0800

    activemq consumer

---
+++ src/main/java/com/nzb/mq/consumer/queue/QueueReceiver1.java
@@ -0,0 +1,13 @@
+package com.nzb.mq.consumer.queue;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+public class QueueReceiver1 implements MessageListener {
+
+	public void onMessage(Message arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
